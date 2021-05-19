70d0b5796213c94022480154a0c518a561fbfd36
WesleyDias28 <wesley.dias28@hotmail.com>
2021-05-18 22:19:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## PUC-Vendas
+<br>
+<p><h1>Grupo:</h1></p>
+<p>Wesley Dias da Silva</p>
+<br>
+<p><h1>Descrição do sistema:</h1></p>
+<p></p>
---
+++ src/main/java/puc/App.java
@@ -0,0 +1,19 @@
+package puc;
+
+/**
+* Esse software foi desenvolvido para resolução do trabalho de Projeto de Sistemas
+* version 1.00 
+* author Wesley Dias
+*
+*/
+public class App {
+    /**
+     * Classe principal.
+     * 
+     *
+     * 
+     */
+    public static void main(String[] args) {
+        
+    }
+}
