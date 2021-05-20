e1bb27c3236cc416389ebd43e7d46172fc50b035
WesleyDias28 <wesley.dias28@hotmail.com>
2021-05-19 23:27:02 -0300

    Adicao de novos testes para cadastro de produtos

--- src/test/java/puc/ProdutosTest.java
+++ src/test/java/puc/ProdutosTest.java
@@ -15,5 +15,18 @@ public class ProdutosTest {
         assertTrue(produtoCadastrado);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void deveRetornarUmaExcecaoParaPrecoMaximoMenorDoQueMinimo() {
+        Produtos produto = new Produtos();
+
+        Boolean produtoCadastrado = produto.cadastrarProdutos(1, "Arroz", 0.0, 0.0, LocalDate.now());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deveRetornarUmaExcecaoParaDataNula() {
+        Produtos produto = new Produtos();
+
+        Boolean produtoCadastrado = produto.cadastrarProdutos(1, "Arroz", 2.0, 3.0, null);
+    }
 
 }
